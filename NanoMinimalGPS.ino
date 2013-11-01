321b5b8c1260468af7fe88bed952c49a3b1dcde5
contractorwolf <contractorwolf@hotmail.com>
2013-11-01 09:48:54 -0400

    cleaned up code, took out all unnecessary comments

--- NanoMinimalGPS.ino
+++ NanoMinimalGPS.ino
@@ -1,5 +1,5 @@
-#include <Adafruit_GFX.h>
-#include <SoftwareSerial.h>
+
+
 
 #include <Time.h>
 #include <Wire.h>
@@ -9,21 +9,9 @@
 #include <SoftwareSerial.h>
 
 /*********************************************************************
-This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
-
-This example is for a 128x64 size display using SPI to communicate
-4 or 5 pins are required to interface
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
+this is the sketch to display the serial GPS stream from the Adafruit GPS breakout 
+on a SSD1306 OLED display using the Arduino Nano.  for questions or comments please 
+contact me at http://google.com/+jameswolf
 *********************************************************************/
 
 
@@ -48,45 +36,22 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-bool inverted = false;
+
 String content = "";
 char character;
 int count = 0;
 int pageRefreshRemaining = 147;
 
-//SoftwareSerial mySerial(8, 7);
-
-
-
-
-
-//Adafruit_GPS GPS(&mySerial);
-
-// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
-// Set to 'true' if you want to debug and listen to the raw GPS sentences. 
-//#define GPSECHO  false
-
-// this keeps track of whether we're using the interrupt
-// off by default!
-//boolean usingInterrupt = false;
-//void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
 
 
 void setup()   {                
   Serial.begin(9600);
-  //Serial.write("MINIMAL application started");
-  
 
-
-  
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC);
   
-  
-  
-  
+
   // init done
-  
   display.clearDisplay();
   
   // text display tests
@@ -96,17 +61,11 @@ void setup()   {
   display.println("MINIMALGPS");
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.println("application started");
-  
+  display.println("nano started");
   
   display.display();
   
-
-  
-
   delay(3000);
-
-
 }
 
 
@@ -114,14 +73,12 @@ void loop() {
   count++;
   display.setTextSize(1);
   display.setTextColor(WHITE);
-    
-    
+   
   content = "";
   while(Serial.available()) {
       character = Serial.read();
       content.concat(character);
       pageRefreshRemaining--;
-      
   }
 
   if (content != "") {
@@ -129,28 +86,21 @@ void loop() {
       display.clearDisplay();
       display.setCursor(0,0);
       pageRefreshRemaining = 147;
-      
-      
-
     }
     
     // text display tests
-
     display.print(content);
-    
     //display.println("length: " + content.length());
-    
-    
+  
     display.display();
-    
-      //delay(200);
+    //delay(200);
   }
   
  // delay(200);
 }
 
 
-
+/*
 String digitalClockDisplay(){
   String display_text = "" + printDigits(minute()) + ":" + printDigits(second());
   return(display_text);
@@ -165,4 +115,4 @@ String printDigits(int digits){
   
   return(val);
 }
-
+*/
